e26d536fb98165ed1864cfaa8523aab01385b964
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-02-11 21:38:39 +0700

    Add Register Authentication

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -44,5 +44,9 @@ class FortifyServiceProvider extends ServiceProvider
         });
 
         Fortify::loginView(fn () => view('pages.auth.login'));
+
+        Fortify::registerView(fn () => view('pages.auth.register'));
+
+
     }
 }
