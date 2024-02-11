24dbcf11b481e0452e5ebb9ad3a14cab66171528
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-02-12 01:11:00 +0700

    Create form menu New Users role admin, staff, user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -23,12 +24,15 @@ class UserController extends Controller
 
     // create
     public function create() {
-        return view('pages.dashboard');
+        return view('pages.user.create');
     }
 
     // store
     public function store(Request $request) {
-        return view('pages.dashboard');
+        $data = $request->all();
+        $data['password'] = Hash::make($request->input('password'));
+        User::create($data);
+        return redirect()->route('user.index');
     }
 
     // show
