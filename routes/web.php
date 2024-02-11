6f53bb0945bf3d1a87138e7033f656214b56ea59
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-02-11 21:30:07 +0700

    Add Laravel Fortify for auth, login ui, dashboard ui and configuration local db user login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth/login');
 });
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('home', function () {
+        return view('pages.dashboard');
+    })->name('home');
+
+
+});
+
+
+// Route::get('/login', function () {
+//     return view('pages.auth.login');
+// });
