c7eae7adcc8e83d9bb1ed99fe06a2d95afcde424
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-02-18 23:06:00 +0700

    API Register Token Is Worked
    Done Test In PostMan Success Token

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        // Validate the request
+        $validated = $request -> validate([
+            'name'  => 'required|max:100',
+            'email' => 'required|email|unique:users|max:100',
+            'password'  => 'required|max:50',
+            'phone' => 'required',
+            'roles'  => 'required',
+
+        ]);
+
+        // Password encryption
+        $validated['password'] = Hash::make( $validated['password'] );
+
+        $user = User::create($validated);
+
+        $token = $user -> createToken('auth_token')->plainTextToken;
+        return response() -> json([
+            'access_token' => $token,
+            'user' => $user,
+        ],201);
+    }
+}
